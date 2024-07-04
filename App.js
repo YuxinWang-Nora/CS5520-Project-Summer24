4687d6ed7c7aaca804200e34e44fe037af6d77c6
Yuxin(Nora) Wang <wang.yuxin5@northeastern.edu>
2024-07-04 14:50:07 -0700

    Add a TextInput to get user's input and a Text component to show the text user has inputed

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Components/Header';
+import React, { useState } from 'react';
 
 export default function App() {
   const appName = "My First React Native App";
+  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
       <Header name={appName} theme="dark">
         <Text>Child1</Text>
         <Text>Child2</Text>
       </Header>
+      <TextInput
+        placeholder='Type here to input!'
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Text>{text}</Text>
+
       <StatusBar style="auto" />
     </View>
   );

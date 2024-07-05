f80284e4d7d1e5b016b17f4262572f93c2aeeb98
Yuxin(Nora) Wang <wang.yuxin5@northeastern.edu>
2024-07-04 21:13:23 -0700

    Update the TextInput component so that it has the focus when the Input component renders

--- Components/Input.js
+++ Components/Input.js
@@ -2,17 +2,26 @@ import { Text, TextInput} from 'react-native';
 import React, {useState} from 'react';
 
 const Input = () => {
-    const [text, setText] = useState('');
+ const [text, setText] = useState('');
+ const [showThankYou, setShowThankYou] = useState(false);
+ 
+ return (
+ <>
+ <TextInput
+ placeholder="Type here to input!"
+ onChangeText={(text) => {
+    setText(text);
+    setShowThankYou(false);
+}}
+value={text}
+autoFocus={true}
+onBlur={() => setShowThankYou(true)}
+onFocus={() => setShowThankYou(false)}
+/>
 
-  return (
-    <>
-    <TextInput
-    placeholder='Type here to input!'
-    onChangeText={text => setText(text)}
-    value={text}/>
-    <Text>{text}</Text>
-    </>
-  );
-}
+{showThankYou && <Text>Thank you.</Text>}
+</>
+ );
+};
 
 export default Input;

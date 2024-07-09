fadb5c416aad65aed2003f312c41ecbdb0f30fee
Yuxin(Nora) Wang <156369896+YuxinWang-Nora@users.noreply.github.com>
2024-07-09 13:24:32 -0700

    merge main

--- Components/Input.js
+++ Components/Input.js
@@ -1,27 +1,26 @@
-import { Text, TextInput} from 'react-native';
-import React, {useState} from 'react';
+import { Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 const Input = () => {
- const [text, setText] = useState('');
- const [showThankYou, setShowThankYou] = useState(false);
- 
- return (
- <>
- <TextInput
- placeholder="Type here to input!"
- onChangeText={(text) => {
-    setText(text);
-    setShowThankYou(false);
-}}
-value={text}
-autoFocus={true}
-onBlur={() => setShowThankYou(true)}
-onFocus={() => setShowThankYou(false)}
-/>
+    const [text, setText] = useState('');
+    const [showThankYou, setShowThankYou] = useState(false);
 
-{showThankYou && <Text>Thank you.</Text>}
-</>
- );
+    return (
+        <>
+            <TextInput
+                placeholder="Type here to input!"
+                onChangeText={(text) => {
+                    setText(text);
+                    setShowThankYou(false);
+                }}
+                value={text}
+                autoFocus={true}
+                onBlur={() => setShowThankYou(true)}
+                onFocus={() => setShowThankYou(false)}
+            />
+
+            {showThankYou && <Text>Thank you.</Text>}
+        </>
+    );
 };
-
 export default Input;

76ea318afd60f304b165906294081a4c8996123e
Yuxin(Nora) Wang <156369896+YuxinWang-Nora@users.noreply.github.com>
2024-07-09 14:39:15 -0700

    Give the View in Input.js container style; Wrap the code in Input.js in a <Modal> component; Use a Modal’s prop to make the Modal slides from the bottom;Add a <Button> titled “Add a goal” in App.js that would make the Modal visible when it's pressed.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import Header from './Components/Header';
 import Input from './Components/Input';
 import React, { useState } from 'react';
@@ -9,18 +9,21 @@ export default function App() {
   const appName = "My First React Native App";
   const [text, setText] = useState('');
   const [receivedText, setReceivedText] = useState('');
+  const [isModuleVisiable, setIsModuleVisiable] = useState(false);
   function handleInputData(data) {
     console.log("callback fn called", data);
     setReceivedText(data);
+    setIsModuleVisiable(false);
   }
 
   return (
     <View style={styles.container}>
-      <Header name={appName} theme="dark">
+      {/* <Header name={appName} theme="dark">
         <Text>Child1</Text>
         <Text>Child2</Text>
-      </Header>
-      <Input inputHandler={handleInputData} />
+      </Header> */}
+      <Input inputHandler={handleInputData} isModuleVisiable={isModuleVisiable} setIsModuleVisiable={setIsModuleVisiable} />
+      <Button title="Add a goal" onPress={() => setIsModuleVisiable(true)} />
       {/* Use the state variable to render the received data*/}
       <Text>{receivedText}</Text>
       <StatusBar style="auto" />
--- Components/Input.js
+++ Components/Input.js
@@ -1,9 +1,11 @@
-import { Button, Text, TextInput } from 'react-native';
+import { Button, Text, TextInput, StyleSheet, View, Modal } from 'react-native';
 import React, { useState } from 'react';
 
 const Input = (props) => {
     const [text, setText] = useState('');
     const [showThankYou, setShowThankYou] = useState(false);
+
+
     const handleConfirm = () => {
         console.log("User has typed", text);
         // Call the received callback function
@@ -11,24 +13,36 @@ const Input = (props) => {
     }
 
     return (
-        <>
-            <TextInput
-                placeholder="Type here to input!"
-                onChangeText={(text) => {
-                    setText(text);
-                    setShowThankYou(false);
-                }}
-                value={text}
-                autoFocus={true}
-                onBlur={() => setShowThankYou(true)}
-                onFocus={() => setShowThankYou(false)}
-            />
+        <Modal animationType='slide' visible={props.isModuleVisiable}>
+            <View style={styles.container}>
+                <TextInput
+                    placeholder="Type here to input!"
+                    onChangeText={(text) => {
+                        setText(text);
+                        setShowThankYou(false);
+                    }}
+                    value={text}
+                    autoFocus={true}
+                    onBlur={() => setShowThankYou(true)}
+                    onFocus={() => setShowThankYou(false)}
+                />
 
-            {showThankYou && <Text>Thank you.</Text>}
-            <Button title="Confirm" onPress={() => {
-                handleConfirm();
-            }} />
-        </>
+                {showThankYou && <Text>Thank you.</Text>}
+                <Button title="Confirm" onPress={() => {
+                    handleConfirm();
+                }} />
+            </View>
+        </Modal>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
 export default Input;

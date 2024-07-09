59ebd44bb00a18275fb19045e994f628a272242d
Yuxin(Nora) Wang <156369896+YuxinWang-Nora@users.noreply.github.com>
2024-07-09 14:01:57 -0700

    Add a Callback function into the APP.js and call the function in the Input.js, and added a Text compoent in the App.js so the data received from Input is shown in the Text component

--- App.js
+++ App.js
@@ -8,13 +8,21 @@ import React, { useState } from 'react';
 export default function App() {
   const appName = "My First React Native App";
   const [text, setText] = useState('');
+  const [receivedText, setReceivedText] = useState('');
+  function handleInputData(data) {
+    console.log("callback fn called", data);
+    setReceivedText(data);
+  }
+
   return (
     <View style={styles.container}>
       <Header name={appName} theme="dark">
         <Text>Child1</Text>
         <Text>Child2</Text>
       </Header>
-      <Input/>
+      <Input inputHandler={handleInputData} />
+      {/* Use the state variable to render the received data*/}
+      <Text>{receivedText}</Text>
       <StatusBar style="auto" />
     </View>
   );
--- Components/Input.js
+++ Components/Input.js
@@ -1,11 +1,13 @@
 import { Button, Text, TextInput } from 'react-native';
 import React, { useState } from 'react';
 
-const Input = () => {
+const Input = (props) => {
     const [text, setText] = useState('');
     const [showThankYou, setShowThankYou] = useState(false);
     const handleConfirm = () => {
-        console.log(text);
+        console.log("User has typed", text);
+        // Call the received callback function
+        props.inputHandler(text);
     }
 
     return (

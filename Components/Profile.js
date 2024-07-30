c14c7b8b5410059b57edfae666a4437b6e6cd897
Yuxin(Nora) Wang <156369896+YuxinWang-Nora@users.noreply.github.com>
2024-07-30 15:04:08 -0700

    Added the users email and id info into the Profile Screen

--- Components/Profile.js
+++ Components/Profile.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { auth } from "../Firebase/firebaseSetup";
+
+const user = auth.currentUser;
 
 export default function Profile() {
+
     return (
         <View style={styles.container}>
             <Text>Profile</Text>
+            <Text>{user.email}</Text>
+            <Text>{user.uid}</Text>
         </View>
     )
 }

c3929e1a3979d0937e82d3ffb2640d49a21b672f
Yuxin(Nora) Wang <156369896+YuxinWang-Nora@users.noreply.github.com>
2024-08-01 15:31:47 -0700

    Moved the const user into the function

--- Components/Profile.js
+++ Components/Profile.js
@@ -2,10 +2,9 @@ import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { auth } from "../Firebase/firebaseSetup";
 
-const user = auth.currentUser;
 
 export default function Profile() {
-
+    const user = auth.currentUser;
     return (
         <View style={styles.container}>
             <Text>Profile</Text>

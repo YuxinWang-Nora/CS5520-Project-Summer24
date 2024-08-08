fc3a4d47c8e484479dd6172aa8d1cc2f3e8552a3
Yuxin(Nora) Wang <156369896+YuxinWang-Nora@users.noreply.github.com>
2024-08-08 14:03:16 -0700

    Completed the activity of Pick A Location on the Map

--- Components/Profile.js
+++ Components/Profile.js
@@ -4,38 +4,8 @@ import * as Location from "expo-location";
 import { auth } from "../Firebase/firebaseSetup";
 import LocationManager from "./LocationManager";
 
-export default function Profile() {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
-    const [status, requestPermission] = Location.useForegroundPermissions();
-
-    const verifyPermission = async () => {
-        if (status.granted) {
-            return true;
-        } else {
-            const permissionResponse = await requestPermission();
-            return permissionResponse.granted;
-        }
-    };
-
-    const locateUserHandler = async () => {
-        const hasPermission = await verifyPermission();
-        if (!hasPermission) {
-            setErrorMsg('Permission to access location was denied');
-            return;
-        }
-
-        try {
-            const location = await Location.getCurrentPositionAsync({});
-            setLocation({
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-            });
-        } catch (err) {
-            setErrorMsg('Error getting location');
-            console.error(err);
-        }
-    };
+export default function Profile({ route }) {
+    const selectedLocation = route.params?.location;
 
     return (
         <View style={styles.container}>
@@ -46,7 +16,7 @@ export default function Profile() {
                         <Text>{auth.currentUser.email}</Text>
                         <Text>{auth.currentUser.uid}</Text>
                         <LocationManager />
-                        {errorMsg && <Text>{errorMsg}</Text>}
+                        {/* {errorMsg && <Text>{errorMsg}</Text>} */}
                     </View>
                 ) : (
                     <Text>No user is currently logged in.</Text>
